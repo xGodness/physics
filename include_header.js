625ffd20340555d8c04aa41105de06462d5f170d
xGodness <alexei.gotovko@gmail.com>
2022-09-15 21:20:38 +0300

    fixing path issues v.2

--- include_header.js
+++ include_header.js
@@ -1,5 +1,5 @@
 async function include() {
-    let header_string = await fetch("../html/header.html").then(html => html.text());
+    let header_string = await fetch("/header.html").then(html => html.text());
     let header = document.getElementsByTagName("header")[0];
     header.innerHTML = header_string + header.innerHTML;
 }
